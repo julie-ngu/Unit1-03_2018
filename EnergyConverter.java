d76bb489de84dd4999ec493ed5c12cb84b5f6f9d
Julie Nguyen <julie@ip-172-31-8-86.ca-central-1.compute.internal>
2018-09-13 18:46:15 +0000

    solution for unit1-03 in java

---
+++ EnergyConverter.java
@@ -0,0 +1,27 @@
+/****************************************************************************
+ *
+ * Created by: Julie Nguyen
+ * Created on: Sept 2018
+ * Created for: ICS4U
+ * This program calculates whatever mass (kg) the user inputs to energy (j)
+ *
+ ****************************************************************************/
+package EnergyConverter;
+import java.util.Scanner;
+
+public class EnergyConverter {
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter mass of object (kg): ");
+		
+		double LIGHTSPEED = 299800000;
+		double mass = userInput.nextDouble();
+		userInput.close();
+		
+		double energy = mass * (LIGHTSPEED * LIGHTSPEED);
+		
+		System.out.println("If " + mass + " kg was converted into energy, " + energy + " joules would be produced.");
+		
+	}
+
+}
